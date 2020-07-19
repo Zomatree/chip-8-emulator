d131f56c2374c8640fd6d57d4af5b678b6eef6cf
Zomatree <39768508+zomatree@users.noreply.github.com>
2020-07-19 23:57:57 +0100

    finish all the op-codes, add logging

--- emulator/cpu.py
+++ emulator/cpu.py
@@ -1,17 +1,45 @@
 from random import randint
-import time
+import logging
 
 
 class CPU:
     def __init__(self, display):
         self.memory = [0]*4096
+        
+        self.memory[0:0x200] = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0,
+            0x20, 0x60, 0x20, 0x20, 0x70,
+            0xF0, 0x10, 0xF0, 0x80, 0xF0,
+            0xF0, 0x10, 0xF0, 0x10, 0xF0,
+            0x90, 0x90, 0xF0, 0x10, 0x10,
+            0xF0, 0x80, 0xF0, 0x10, 0xF0,
+            0xF0, 0x80, 0xF0, 0x90, 0xF0,
+            0xF0, 0x10, 0x20, 0x40, 0x40,
+            0xF0, 0x90, 0xF0, 0x90, 0xF0,
+            0xF0, 0x90, 0xF0, 0x10, 0xF0,
+            0xF0, 0x90, 0xF0, 0x90, 0x90,
+            0xE0, 0x90, 0xE0, 0x90, 0xE0,
+            0xF0, 0x80, 0x80, 0x80, 0xF0,
+            0xE0, 0x90, 0x90, 0x90, 0xE0,
+            0xF0, 0x80, 0xF0, 0x80, 0xF0,
+            0xF0, 0x80, 0xF0, 0x80, 0x80,
+        ]
+
         self.registers = [0]*16
         self.stack = [0]*16
         self.stack_pointer = 0
         self.delay_timer = 0
+        self.sound_timer = 0
         self.index = 0
-        self.pc = 0
+        self.pc = 0x200
         self.display = display
+
+    def load_program(self, filename):
+        with open(filename, "rb") as f:
+            logging.info(f"Reading file {filename}")
+            data = f.read()
+            for index, byte in zip(range(len(data)), data):
+                self.memory[0x200+index] = byte
 
     def add_overflow(a, b):
         result = a + b
@@ -24,18 +52,21 @@ class CPU:
         return result, not bool(result % 8)
 
     def read_word(self):
-        return self.memory[self.index] << 8 | self.memory[self.index+1]
+        return self.memory[self.pc] << 8 | self.memory[self.pc+1]
 
     def exec_cycle(self):
         opcode = self.read_word()
         self.process_opcode(opcode)
 
     def process_opcode(self, opcode):
+        logging.info(f"processing opcode {opcode}")
         x = (opcode & 0x0F00) >> 8
-        y = (opcode & 0x0F00) >> 4
+        y = (opcode & 0x00F0) >> 4
 
-        vx = self.registers[x]
-        vy = self.registers[y]
+        logging.info(f"{x=}, {y=}")
+
+        vx = int(str(self.registers[x]), 16)
+        vy = int(str(self.registers[y]), 16)
 
         nnn = opcode & 0x0FFF
         nn = opcode & 0x00FF
@@ -50,15 +81,18 @@ class CPU:
 
         ops = (op_1, op_2, op_3, op_4)
 
+        logging.info(f"{ops=}")
+
         if ops == (0, 0, 0xE, 0):
             self.display.clear()
         if ops == (0, 0, 0xE, 0xE):
             self.stack_pointer -= 1
+            self.pc = self.stack[self.stack_pointer]
         if op_1 == 1:
             self.pc = nnn
         if op_1 == 2:
-            self.stack[self.sp] = self.pc
-            self.sp += 1
+            self.stack[self.stack_pointer] = self.pc
+            self.stack_pointer += 1
             self.pc = nnn
         if op_1 == 3:
             if vx == nn:
@@ -124,9 +158,51 @@ class CPU:
             collision = self.display.draw(vx, vy, self.memory[self.index:n*8])
             self.registers[0xF] = collision
 
+        if op_1 == 0xE and op_3 == 0x9 and op_4 == 0xE:
+            if vx == self.display.keypress:
+                self.pc += 2
+
+        if op_1 == 0xE and op_3 == 0x9 and op_4 == 0xE:
+            if vx != self.display.keypress:
+                self.pc += 2
+
+        if op_1 == 0xF and op_3 == 0x0 and op_4 == 0x7:
+            self.registers[x] = self.delay_timer
+
+        if op_1 == 0xF and op_3 == 0x0 and op_4 == 0xA:
+            self.registers[x] = self.display.wait_for_keypress()
+
+        if op_1 == 0xF and op_3 == 0x0 and op_4 == 0x7:
+            self.delay_timer = vx
+
+        if op_1 == 0xF and op_3 == 0x1 and op_4 == 0x8:
+            self.sound_timer = vx
+
+        if op_1 == 0xF and op_3 == 0x1 and op_4 == 0xE:
+            self.index += vx
+
+        if op_1 == 0xF and op_3 == 0x1 and op_4 == 0xE:
+            self.index = vx * 4
+        
+        if op_1 == 0xF and op_3 == 0x3 and op_4 == 0x3:
+            self.memory[self.index] = vx / 100
+            self.memory[self.index + 1] = (vx / 10) % 10
+            self.memory[self.index + 2] = (vx % 100) % 10
+
+        if op_1 == 0xF and op_3 == 0x5 and op_4 == 0x5: 
+            v = self.registers[:x]
+            self.memory[self.index:self.index+len(v)] = v
+
+        if op_1 == 0xF and op_3 == 0x6 and op_4 == 0x5:
+            self.registers[:x] = self.memory[self.index:self.index+x]
+
     def decrement_timer(self):
         if self.delay_timer:
             self.delay_timer -= 1
+
+        if self.sound_timer:
+            self.sound_timer -= 1
+            self.display.beep()
 
     def mainloop(self):
         while True:
@@ -137,4 +213,4 @@ class CPU:
 
             self.display.update()
             self.display.sleep()
-            self.display.clear_screen()
+            self.display.clear()
--- emulator/display.py
+++ emulator/display.py
@@ -1,21 +1,29 @@
 import os
+import logging
+import sys
+
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "True"
 
 import pygame
-import sys
 
 pygame.display.init()
+pygame.mixer.init()
+
+pygame.mixer.music.load('beep.wav')
 fps = pygame.time.Clock()
 pygame.display.set_caption("CHIP-8 Emulator")
 
 
 class Display:
-    def __init__(self, pixel_size=16):
+    def __init__(self, pixel_size, keyboard):
         self.pixel_size = pixel_size
         self.screen = self.blank_screen
         self.keypress = None
+        self.keyboard = {getattr(pygame, f"{k[0].upper()}{k[1:]}"): v for k, v in keyboard.items()}
+
         self.surface = pygame.display.set_mode(size=(64*pixel_size, 32*pixel_size))
-    
+        self.beep = pygame.mixer.music.play
+
     def draw(self, vx, vy, pixels):
         """
         Draws a sprite at coordinate (VX, VY)
@@ -25,14 +33,16 @@ class Display:
         As described above, VF is set to 1 if any screen pixels are flipped
         from set to unset when the sprite is drawn or to 0 if that doesnt happen
         """
-        collision = False
+        collision = 0
     
-        for i in range(0, len(pixels), 8):
-            new_row = pixels[i:i+8]
-            old_row = self.screen[vy + int(i / 8)][vx:vx+8]
-            
-            collision = any(True for before, after in zip(old_row, new_row) if before == 1 and after == 0)
-            self.screen[vy + int(i / 8)][vx:vx+8] = pixels[i:i+8]
+        for i in range(0, len(pixels)):
+            for new_pixel in pixels[i * 8:(8 * i) + 8]:
+                before = self.screen[vy + i][vx + (i * 8)]
+                pixel = (new_pixel == 1) ^ before
+                if before == 1 and pixel == 0:
+                    collision = 1
+
+                self.screen[vy + i][vx+(i*8)] = pixel
     
         return int(collision)
     
@@ -46,9 +56,16 @@ class Display:
             for j, value in enumerate(row):
                 colour = (value*255, value*255, value*255)
                 pygame.draw.rect(self.surface, colour, pygame.Rect(j*self.pixel_size, i*self.pixel_size, self.pixel_size, self.pixel_size))
-
         pygame.display.update()
-
+    
+    def set_keypress(self):
+        pygame.event.pump()
+        keys = pygame.key.get_pressed()
+        for key, hex_value in self.keyboard.items():
+            if keys[key]:
+                self.keypress = hex_value
+                logging.info(f"Detected keypress {key} {hex_value=}")
+ 
     @property
     def blank_screen(self):
         return [[0 for _ in range(64)] for _ in range(32)]
@@ -57,5 +74,13 @@ class Display:
     def sleep():
         fps.tick(60)
 
-    def clear_screen(self):
+    def clear(self):
         self.surface.fill((0,0,0))
+
+    def wait_for_keypress(self):
+        self.keypress = None
+        while not self.keypress:
+            self.set_keypress()
+            self.sleep()
+
+        return self.keypress
